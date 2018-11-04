6352848f0c2635e85a85a0403c30f41ac7a378d6
tweak222 <tylertechseed@gmail.com>
2018-11-03 20:04:43 -0400

    Continuing Course

---
+++ src/components/contacts/AddContact.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class AddContact extends Component {
+  state = {
+    name: '',
+    email: '',
+    phone: ''
+  };
+
+  render() {
+    return (
+      <div className="card mb-3">
+        <div className="card-header">Add Contact</div>
+      </div>
+    );
+  }
+}
+
+export default AddContact;
